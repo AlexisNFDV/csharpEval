3077f3f491f230e9eab4882e7ae8527cd875913a
AlexisNFDV <alexis.noel@ynov.com>
2020-10-22 10:54:02 +0200

    Login view + controller

---
+++ csharpdev/Venezia/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Venezia.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("Connexion", Name = "LoginUrl")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
